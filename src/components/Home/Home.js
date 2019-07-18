60f96cc6b28f3790f86c5ec0e66ad0dae0b81e18
wangyulin <1404939347@qq.com>
2019-07-18 19:39:35 +0800

    name update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,7 +4,7 @@ import fighting from './fighting.jpg';
 const Home = () => {
 	return (
 		<div className=" ">
-			<div className="f1 cl"><h1>Aaron <span className="cs">Wang</span></h1></div>
+			<div className="f1 cl"><h1>Yulin <span className="cs">Wang</span></h1></div>
 			<div className="f2 cl"><p>Problem Solver</p></div>
 			<div><img src={fighting} alt=""/></div>
 
